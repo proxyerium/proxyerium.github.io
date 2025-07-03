0f2f2f1d116a820ab3b4407c530412a822763118
proxyerium <proxyerium@outlook.com>
2025-07-03 12:19:12 +0800

    add: code highlight

--- _config.ts
+++ _config.ts
@@ -1,12 +1,29 @@
 import lume from 'lume/mod.ts';
 import date from 'lume/plugins/date.ts';
 import paginate from 'lume/plugins/paginate.ts';
+import codeHighlight from 'lume/plugins/code_highlight.ts';
 
 const site = lume({
   src: '/src/',
 })
   .add('/styles.css')
   .use(date())
-  .use(paginate());
+  .use(paginate())
+  .use(
+    codeHighlight({
+      theme: [
+        {
+          name: 'atom-one-light',
+          cssFile: '/styles.css',
+          placeholder: '/* light-theme-here */',
+        },
+        {
+          name: 'atom-one-dark',
+          cssFile: '/styles.css',
+          placeholder: '/* dark-theme-here */',
+        },
+      ],
+    })
+  );
 
 export default site;
